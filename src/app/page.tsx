3260c5be7258e7f282699709b520fb64804895e3
Maycon Kabitschke <mayconkabitschke@gmail.com>
2024-08-03 05:38:53 -0300

    arrayState

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,26 @@ const Page = () => {
     checked: boolean;
   }
 
-  const [list, setList] = useState<Todo[]>([{label: 'Fazer dever de casa', checked: false}, {label: 'Fazer dever de casa', checked: true}]);
+  const [itemInput, setItemInput] = useState('');
+  const [list, setList] = useState<Todo[]>([]);
 
 
+const handleAddButton = () => {
+  if(itemInput.trim() === '')return;
+  setList([...list, {label: itemInput, checked:false}]);
+  setItemInput('');  
+}
 
+const deleteItem = (e:number) => {
+  setList(list.filter((el, key)=> key !== e));
+}
 
+const toggleItem = (index: number) => {
+  const newList = list.map((item, i) => 
+    i === index ? { ...item, checked: !item.checked } : item
+  );
+  setList(newList);
+}
 
   return (
     <div className="w-screen h-screen flex flex-col items-center text-2xl">
@@ -24,18 +39,37 @@ const Page = () => {
         <input type="text"
         placeholder="O que deseja fazer"
         className="flex-1 boder border-black p-3 text-2xl text-black rounded-md mr-3" 
+        value={itemInput}
+        onChange={e => setItemInput(e.target.value)}
         />
 
-      <button>Adicionar</button>
+      <button className="bg-green-400 p-3 text-white rounded-md" onClick={handleAddButton}>Adicionar</button>
       </div>
 
-      <ul>
-        {list.map(el => (
-        <li>{el.label}  - <button className="hover:underline">[ deletar ]</button></li>
-        
-      ))}
-      
+
+    <div className="flex w-full max-w-lg my-3 bg-slate-300">
+      <ul className="w-full">
+        {list.map((el, i) => (
+          <li key={i} className="flex justify-between items-center p-2 border-b border-slate-400">
+            <input onClick={() => toggleItem(i)} type="checkbox" checked={el.checked}  className="w-6 h-6 mr-3"/>
+            <span
+                onClick={() => toggleItem(i)}
+                className={`flex-1 cursor-pointer ${el.checked ? 'line-through' : ''}`}
+              >
+                {el.label}
+              </span>
+
+
+            <button 
+            className="hover:underline p-2 bg-red-500 rounded-md text-white text-xs"
+            onClick={() => deleteItem(i)}
+            
+            >Deletar</button>
+          </li>
+        ))}
       </ul>
+    </div>
+
 
 
     </div>
